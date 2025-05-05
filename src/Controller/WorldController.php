fb3ee37782861fa90d6aac0b1eb62c30fe5f0f53
Konstantin Savin <ks5470219@gmail.com>
2025-05-05 18:44:55 +0700

    init

---
+++ src/Controller/WorldController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class WorldController extends AbstractController
+{
+
+
+    public function hello(): int
+    {
+
+
+        return $a = 1;
+    }
+}
